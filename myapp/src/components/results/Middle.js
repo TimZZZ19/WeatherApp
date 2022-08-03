7c8fb2b5a04982e32b47a77ef94984197cdcd6c2
TimZ <tiomsity@gmail.com>
2022-08-03 15:39:53 -0500

    hard-coded the middle section of reports

--- myapp/src/components/results/Middle.js
+++ myapp/src/components/results/Middle.js
@@ -2,7 +2,18 @@ import React from "react";
 import styles from "./Middle.module.css";
 
 const Middle = () => {
-  return <div className={styles.middle}></div>;
+  return (
+    <div className={styles.middle}>
+      <div className={styles.sunrise}>
+        <p className={styles.time}>07:00 am</p>
+        <p className={styles.text}>Sunrise</p>
+      </div>
+      <div className={styles.sunset}>
+        <p className={styles.time}>08:00 pm</p>
+        <p className={styles.text}>Sunset</p>
+      </div>
+    </div>
+  );
 };
 
 export default Middle;
