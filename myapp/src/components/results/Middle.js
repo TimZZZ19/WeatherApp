f1462712b7002ca8589ec55a5ebb155953740b55
TimZ <tiomsity@gmail.com>
2022-08-03 17:43:09 -0500

    finished hard-coded reports

--- myapp/src/components/results/Middle.js
+++ myapp/src/components/results/Middle.js
@@ -5,12 +5,16 @@ const Middle = () => {
   return (
     <div className={styles.middle}>
       <div className={styles.sunrise}>
-        <p className={styles.time}>07:00 am</p>
-        <p className={styles.text}>Sunrise</p>
+        <p className={styles.text}>
+          <i className="bi bi-sunrise"></i>
+        </p>
+        <span className={styles.time}>07:00 am</span>
       </div>
       <div className={styles.sunset}>
-        <p className={styles.time}>08:00 pm</p>
-        <p className={styles.text}>Sunset</p>
+        <p className={styles.text}>
+          <i className="bi bi-sunset"></i>
+        </p>
+        <span className={styles.time}>08:00 pm</span>
       </div>
     </div>
   );
