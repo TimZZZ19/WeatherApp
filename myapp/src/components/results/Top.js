256cd542da463ad94c9b0f30bfd9c069df2041fd
TimZ <tiomsity@gmail.com>
2022-08-03 15:24:15 -0500

    hard-coded the top part of reports

--- myapp/src/components/results/Top.js
+++ myapp/src/components/results/Top.js
@@ -2,7 +2,21 @@ import React from "react";
 import styles from "./Top.module.css";
 
 const Top = () => {
-  return <div className={styles.top}></div>;
+  return (
+    <div className={styles.top}>
+      <div className={styles["location-converter"]}>
+        <span className={styles.location}>Chicago</span>
+        <div className={styles.converter}>
+          <button className={styles.fahrenheit}>&#176;F</button>
+          <button className={styles.celsius}>&#176;C</button>
+        </div>
+      </div>
+      <div className={styles["temperature-weather-condition"]}>
+        <span className={styles.temperature}>81&#176;F</span>
+        <span className={styles["weather-condition"]}>Clear</span>
+      </div>
+    </div>
+  );
 };
 
 export default Top;
