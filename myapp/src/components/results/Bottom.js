8b11b3d2c84d91b6f39c8166efa1fa1f038c4548
TimZ <tiomsity@gmail.com>
2022-08-03 16:02:17 -0500

    hard-coded the bottom section of reports

--- myapp/src/components/results/Bottom.js
+++ myapp/src/components/results/Bottom.js
@@ -2,7 +2,22 @@ import React from "react";
 import styles from "./Bottom.module.css";
 
 const Bottom = () => {
-  return <div className={styles.bottom}></div>;
+  return (
+    <div className={styles.bottom}>
+      <div className={styles["feels-like"]}>
+        <p className={styles.number}>84&#176;F</p>
+        <p className={styles.text}>Feels Like</p>
+      </div>
+      <div className={styles["humidity"]}>
+        <p className={styles.number}>65%</p>
+        <p className={styles.text}>Humidity</p>
+      </div>
+      <div className={styles["wind-speed"]}>
+        <p className={styles.number}>4MPH</p>
+        <p className={styles.text}>Wind Speed</p>
+      </div>
+    </div>
+  );
 };
 
 export default Bottom;
