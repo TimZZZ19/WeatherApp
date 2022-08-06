2e5ef9dd6b2586362134234873bc8b8747be2ca6
TimZZZ <tiomsity@gmail.com>
2022-08-06 12:00:22 -0500

    finished

--- myapp/src/components/results/Middle.js
+++ myapp/src/components/results/Middle.js
@@ -1,41 +1,57 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styles from "./Middle.module.css";
 
-const localTimeDiffToUTC = new Date().getTimezoneOffset() * 60;
 
-const convertCutOffTime = (timestamp, locationTimeDiffToUTF) => {
+const normalizeTime = (timestamp, locationTimeDiffToUTF) => {
+  const localTimeDiffToUTC = new Date().getTimezoneOffset() * 60;
+
   const time = new Date(
     (timestamp + localTimeDiffToUTC + locationTimeDiffToUTF) * 1000
   );
 
-  const hours = time.getHours();
-  const mins = time.getMinutes();
+  const result = time.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+   });
+ 
+   return result;
+  // const hours = time.getHours();
+  // const mins = time.getMinutes();
 
-  const normalizedHours = hours > 12 ? hours - 12 : hours;
+  // const normalizedHours = hours > 12 ? hours - 12 : hours;
 
-  const hoursString =
-    normalizedHours < 10 ? `0${normalizedHours}` : `${normalizedHours}`;
+  // const hoursString =
+  //   normalizedHours < 10 ? `0${normalizedHours}` : `${normalizedHours}`;
 
-  const minsString = mins < 10 ? `0${mins}` : `${mins}`;
-  const periodString = hours < 12 ? "am" : "pm";
+  // const minsString = mins < 10 ? `0${mins}` : `${mins}`;
+  // const periodString = hours < 12 ? "am" : "pm";
 
-  return hoursString + ":" + minsString + " " + periodString;
+  // return hoursString + ":" + minsString + " " + periodString;
 };
 
-const getCurrentTime = (locationTimeDiffToUTF) => {
-  const locationTime = new Date(
-    (Date.now() / 1000 + localTimeDiffToUTC + locationTimeDiffToUTF) * 1000
-  );
-
-  console.log(locationTime);
-};
 
 const Middle = ({
   sunriseTimestamp,
   sunsetTimestamp,
   locationTimeDiffToUTF,
 }) => {
-  getCurrentTime(locationTimeDiffToUTF);
+
+  const [currentLocationTime, setCurrentLocationTime] = useState('');
+
+  useEffect(() => {
+    const clockId = setInterval(() => tick(), 1000)
+
+    return function cleanup () {
+      clearInterval(clockId);
+    }
+  })
+
+  function tick(){
+    const time = normalizeTime(Date.now() / 1000, locationTimeDiffToUTF);
+    setCurrentLocationTime(time);
+  }
+
 
   return (
     <div className={styles.middle}>
@@ -44,20 +60,18 @@ const Middle = ({
           <i className="bi bi-sunrise"></i>
         </p>
         <span className={styles.time}>
-          {convertCutOffTime(sunriseTimestamp, locationTimeDiffToUTF)}
+          {normalizeTime(sunriseTimestamp, locationTimeDiffToUTF)}
         </span>
       </div>
       <div className={styles.clock}>
-        <span className={styles["location-time"]}>11 : 00</span>
-        &nbsp;
-        <span className={styles["location-period"]}>am</span>
+        <span className={styles["location-time"]}>{currentLocationTime}</span>
       </div>
       <div className={styles.sunset}>
         <p className={styles.text}>
           <i className="bi bi-sunset"></i>
         </p>
         <span className={styles.time}>
-          {convertCutOffTime(sunsetTimestamp, locationTimeDiffToUTF)}
+          {normalizeTime(sunsetTimestamp, locationTimeDiffToUTF)}
         </span>
       </div>
     </div>
